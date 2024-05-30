d7506aba74b345ceff856a2d144dacbe3aca237d
Flavio Milani <frmilanij@gmail.com>
2024-05-30 02:26:22 -0300

    Correction of heritage examples

--- src/edu/flaviomxx/pilarespoo/heranca/ServicoMensagemInstantanea.java
+++ src/edu/flaviomxx/pilarespoo/heranca/ServicoMensagemInstantanea.java
@@ -7,7 +7,7 @@ public class ServicoMensagemInstantanea { //Todas as classes que extends essa pa
         salvarHistoricoMensagem();
     }
     public void receberMensagem() {
-        System.out.println("Recebendo mensagem pelo Facebook");
+        System.out.println("Recebendo mensagem");
     }
     private void validarConectadoInternet() {
         System.out.println("Validando se está conectado a internet");
